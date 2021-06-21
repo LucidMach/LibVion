829a6aecf12a40a80c4a22e2d0de1b218e6ff28c
LucidMach <nukalasuraj7@gmail.com>
2021-06-21 12:47:43 +0530

    menu dot animations

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,8 +1,14 @@
 import "./navbar.css";
-import React from "react";
+import React, { useRef } from "react";
 import Toggle from "./toggle";
 
 const NavBar = ({ logo, theme, setTheme }) => {
+  const menu = useRef();
+
+  const handleClick = (e) => {
+    menu.current.classList.toggle("open");
+  };
+
   return (
     <div className="nav">
       <svg
@@ -11,15 +17,19 @@ const NavBar = ({ logo, theme, setTheme }) => {
         height="25"
         viewBox="0 0 24 24"
         fill="#00cbcb"
+        onClick={handleClick}
+        ref={menu}
       >
         <path d="M6 12c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3zm9 0c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3zm9 0c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3z" />
       </svg>
+
       <img
         src={logo}
         alt="App Logo"
         width="60"
         style={{ background: "none" }}
       />
+
       <Toggle theme={theme} setTheme={setTheme}></Toggle>
     </div>
   );
