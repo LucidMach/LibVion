b0d619dd54d133735dfc344efe121873b4fd5513
LucidMach <nukalasuraj7@gmail.com>
2021-07-13 11:25:59 +0530

    spinner

--- client/src/components/spinner.js
+++ client/src/components/spinner.js
@@ -2,17 +2,8 @@ import "./spinner.css";
 
 const Loading = () => {
   return (
-    <div className="App">
-      <p style={{ color: "#9b78f2", margin: "1rem", textAlign: "justify" }}>
-        <strong>
-          NOTE : if the app takes too long to respond, you probabily have a typo
-          in the city name.
-        </strong>
-      </p>
+    <div style={{ display: "flex", justifyContent: "center" }}>
       <div className="Loading"></div>
-      <button className="btn" onClick={() => window.location.reload()}>
-        CANCEL
-      </button>
     </div>
   );
 };
