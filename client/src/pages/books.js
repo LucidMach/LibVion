639f69dd2a66770a033b4b77a6855d4448bc6a97
LucidMach <nukalasuraj7@gmail.com>
2021-07-15 14:38:05 +0530

    books UI reset

--- client/src/pages/books.js
+++ client/src/pages/books.js
@@ -5,44 +5,25 @@ const Books = () => {
   const [books, setBooks] = useState([]);
 
   useEffect(() => {
-    fetch('/books/me')
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
+    fetch("/books/me")
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data);
       });
   }, []);
 
   return (
     <>
-    <React.Fragment>
-      <ExtendDrop msg="my books" default={true}>
-        {
-          books ?
-          books.map((book, index) => 
-            <div key={index} style={bookStyle}>
-              <h1>{book.name}</h1>
-              <p>{book.author}</p>
-            </div>  
-          ) :
-          "no books issued"
-        }
-      </ExtendDrop>
-      <ExtendDrop msg="my requests" default={false}>
-        REQUESTS
-      </ExtendDrop>
-    </React.Fragment>
+      <div className="card">
+        <ExtendDrop msg="my books" default={true}>
+          {JSON.stringify(books.books)}
+        </ExtendDrop>
+        <ExtendDrop msg="my requests" default={false}>
+          REQUESTS
+        </ExtendDrop>
+      </div>
     </>
   );
 };
 
 export default Books;
-
-const bookStyle = {
-  display: "grid",
-  gap: ".5em",
-  padding: "8px 16px",
-  margin: "5px 0",
-  border: "1px solid lightgrey",
-  width: "max-content",
-  textAlign: "center"
-}
